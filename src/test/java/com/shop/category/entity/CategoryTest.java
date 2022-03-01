896b25d568af5d9bfe2dfd04ad6ecb925d3da728
jade <weleek@gmail.com>
2022-03-01 11:08:12 +0900

    fix: jUnit4에서 jUnit5로 의존성 변경

--- src/test/java/com/shop/category/entity/CategoryTest.java
+++ src/test/java/com/shop/category/entity/CategoryTest.java
@@ -3,12 +3,10 @@ package com.shop.category.entity;
 import com.shop.category.repository.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
